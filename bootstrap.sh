62e99e23e254f64fc1aed0d4e4f9877ac23add1c
Michael <Ifiht@users.noreply.github.com>
2021-01-23 23:59:00 -0500

    zerotier init updates
    change to wget'ing the tar directly, join automatically.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,30 @@
 #!/bin/sh
 set -u
 
+wget https://github.com/zerotier/ZeroTierOne/archive/1.6.2.tar.gz
+tar -xzf 1.6.2.tar.gz
+cd ZeroTierOne-1.6.2
+
 os=`uname`;
 # First check what OS we're on:
 case "$os" in
 	macos)
 		echo "Assuming host is OS X";
+		gmake; # compiles zerotier-one
 		os=1;
 	;;
 	linux)
 		echo "Assuming host is Linux";
 		sudo apt update && sudo apt upgrade -y;
-		sudo apt install ant build-essential clang cloud-init coreutils dbus dbus-x11 emacs figlet file findutils fortune-mod gcc geoip-database git gnupg gzip leiningen less lsb-base lsb-release lshw lsof make multiarch-support nano ncurses-base net-tools openjdk-8-jdk pngcrush python3.6 rand readline-common screen tcpdump telnet tmux toilet xauth xdelta3 xfsprogs zsh;
+		sudo apt install clang coreutils file findutils fortune-mod gcc geoip-database git gnupg gzip less lsb-base lsb-release lshw lsof make multiarch-support nano ncurses-base net-tools openjdk-8-jdk pngcrush python3.6 rand readline-common screen tcpdump telnet xfsprogs zsh;
+		make; # compiles zerotier-one
 		os=2;
 	;;
 	*Microsoft)
 		echo "Assuming host is WSL";
 		sudo apt update && sudo apt upgrade -y;
-		sudo apt install ant build-essential clang cloud-init coreutils dbus dbus-x11 emacs figlet file findutils fortune-mod gcc geoip-database git gnupg gzip leiningen less lsb-base lsb-release lshw lsof make multiarch-support nano ncurses-base net-tools openjdk-8-jdk pngcrush python3.6 rand readline-common screen tcpdump telnet tmux toilet xauth xdelta3 xfsprogs zsh;
+		sudo apt install clang coreutils file findutils fortune-mod gcc geoip-database git gnupg gzip less lsb-base lsb-release lshw lsof make multiarch-support nano ncurses-base net-tools openjdk-8-jdk pngcrush python3.6 rand readline-common screen tcpdump telnet xfsprogs zsh;
+		make; # compiles zerotier-one
 		os=2;
 	;;
 	*BSD)
@@ -26,13 +33,13 @@ case "$os" in
 		sudo pkg upgrade;
 		sudo portsnap auto;
 		sudo pkg install git gmake gnupg zsh bash nano ruby screen;
+		gmake; # compiles zerotier-one
 		os=3;
 	;;
 	*)
 	exit 1
 esac
 
+sudo zerotier-one -d
+sudo zerotier-cli join 12ac4a1e7110d239
 hash -r
-git clone https://github.com/Ifiht/ZeroTierOne.git
-cd ZeroTierOne
-git checkout prod
